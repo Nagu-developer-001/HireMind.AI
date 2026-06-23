5216cd39d7e43b4c550f1af4e7a364f1c5c62221
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2026-06-23 23:35:12 +0530

    Latest auth changes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -60,7 +60,7 @@ exports.register = async (req, res) => {
   } catch (error) {
     console.error('Register error:', error);
     res.status(500).json({
-      success: true,
+      success: false,
       error: error.message || 'Server error during registration'
     });
   }
