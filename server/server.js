44a6d9911004ba155e259f034bbf4e99e66abdaf
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2026-07-21 22:21:08 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ console.log(
 // 1. MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-
+console.log("CORS and JSON middleware enabled.");
 // 2. MONGODB CONNECTION
 mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/placement_db')
   .then(() => console.log('✅ Connected to MongoDB'))
